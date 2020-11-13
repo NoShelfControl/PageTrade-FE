81b0d203c3d3e1a4bddcfee56ab3785861404375
ryan diffenbaugh <ryandiffenbaugh@gmail.comgit config --global user. name Ryan-Diffgit config --global user.email ryandiffenbaugh@gmail.com>
2020-11-13 13:23:31 -0800

    Added signup form

---
+++ src/context/AuthContext.js
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react';
+
+export const useSignup = () => {
+    const { signup } = useContext(AuthContext);
+    return signup;
+};
+
+export const useLogin = () => {
+    const { login } = useContext(AuthContext);
+    return login;
+};
+
+export const useCurrentUser = () => {
+    const { currentUser } = useContext(AuthContext);
+    return currentUser;
+};
+
+export const useAuthLoading = () => {
+    const { loading } = useContext(AuthContext);
+    return loading;
+};
