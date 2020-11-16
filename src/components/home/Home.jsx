3dfd5b50731f766bde626e016e3d63b6a31cbfe2
Heather Peterson <hpeterson462@gmail.com>
2020-11-16 11:39:47 -0800

    basic layout

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,29 @@
-  import React from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Home.css';
 
-const Home = () => (
-  <h1>Home Page</h1>
-);
+export default function Home() {
 
-export default Home;
+  return (
+    <div className={styles.Home}>
+      <span className={styles.NavButtonToggle}></span>
+      <header>
+        <button>Home</button>
+        <button>Library</button>
+        <button>Requests</button>
+        <button>Profile</button>
+      </header>
+      <main className={styles.Main}>
+        <section className={styles.BookSection}>
+          <div>Trade</div>
+          <div>Wish List</div>
+        </section>
+        <div>Feed</div>
+      </main>
+      <footer>
+        <Link to={`/about`}>About</Link>
+      </footer>
+    </div>
+  )
+}
+
