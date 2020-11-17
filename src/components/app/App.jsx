6fc1eb3f360718ad62242361ef5a86247e1b4dd6
Heather Peterson <hpeterson462@gmail.com>
2020-11-17 09:50:45 -0800

    App.jsx routes updated

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -15,11 +15,12 @@ export default function App() {
   return (
     <AuthProvider>
       <Switch>
-        <Route exact path="/" component={Home} />
+        <PrivateRoute exact path="/" component={Home} />
         <Route exact path="/signup" component={Signup} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/library" component={Library} />
-        <PrivateRoute exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/profile" component={Profile} />
+        <Route exact path="/about" component={About} />
       </Switch>
     </AuthProvider>
   );
