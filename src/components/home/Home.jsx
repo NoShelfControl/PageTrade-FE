48b197c8fe077c936300d8790abc718222f55928
Heather Peterson <hpeterson462@gmail.com>
2020-11-16 16:56:27 -0800

    book covers rendering to trade div

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,31 +1,50 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 import styles from './Home.css';
 import Sidebar from '../sidebar/Sidebar';
+import { getUserBooks } from '../../services/books-api';
 
 export default function Home() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    getUserBooks()
+      .then(books => setBooks(books))
+      .then(books => console.log(books))
+  }, []);
+
+  const booksElements = books.map(book => (
+    <ul>
+      <li key={books.id}>
+        <img src={book.image} alt={book.title} />
+        <p>{book.isTradeable}</p>
+      </li>
+    </ul>
+  ))
+
+  // if (books.isTradeable === true) {
+  //   return books
+  // }
 
   return (
-    <div className={styles.Home}>
-      <section className={styles.SideNavBar}>
-        <Sidebar />
+    <div className={styles.Home} id="outer-container">
+      <section className={styles.SideNavBar} id={styles.PageWrap}>
+        {/* <Sidebar pageWrapId={'PageWrap'} outerContainerId={'outer-container'} /> */}
         <header>
           <button>Home</button>
           <button>Library</button>
           <button>Requests</button>
           <button>Profile</button>
         </header>
+
+        <main className={styles.Main}>
+
+          <section className={styles.BookSection}>
+            <div>Trade {booksElements}</div>
+            <div>Wish List</div>
+          </section>
+          <div>Feed</div>
+        </main>
       </section>
-      <main className={styles.Main}>
-        <section className={styles.BookSection}>
-          <div>Trade</div>
-          <div>Wish List</div>
-        </section>
-        <div>Feed</div>
-      </main>
-      <footer>
-        <Link to={`/about`}>About</Link>
-      </footer>
     </div>
   )
 }
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import { slide as Menu } from 'react-burger-menu';
+import styles from './Sidebar.css';
 
 export default function Sidebar() {
   return (
     <Menu>
-      <a href="/" className="nav-item">
+      <a href="/" className={styles.NavItem}>
         Home
       </a>
-      <a href="/library" className="nav-item">
+      <a href="/library" className={styles.NavItem}>
         Library
       </a>
-      <a href="/profile" className="nav-item">
+      <a href="/profile" className={styles.NavItem}>
         Profile
       </a>
-      <a href="/requests" className="nav-item">
+      <a href="/requests" className={styles.NavItem}>
         Requests
       </a>
-      <a href="/about" className="nav-item">
+      <a href="/about" className={styles.NavItem}>
         About
       </a>
     </Menu>
