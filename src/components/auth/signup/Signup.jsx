7e7ea385505051a96bc0fc323770d620628a5fe2
brookeperkins <brookemmperkins@gmail.com>
2020-11-17 09:43:18 -0800

    styles

--- src/components/auth/signup/Signup.jsx
+++ src/components/auth/signup/Signup.jsx
@@ -1,6 +1,10 @@
+/* eslint-disable max-len */
 import React from 'react';
 import { useState } from 'react';
 import { useSignup } from '../../../context/AuthContext';
+import styles from './Signup.css';
+import logo from '../../../assets/logo.png';
+import trackbooks from '../../../assets/trackbooks.png';
 
 const Signup = () => {
   const [email, setEmail] = useState('');
@@ -13,21 +17,32 @@ const Signup = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={({ target }) => setEmail(target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={({ target }) => setPassword(target.value)}
-      />
-      <button>Signup</button>
-    </form>
+    <main>
+      <section className={styles.signupcontainer}>
+        <img id={styles.logo} src={logo} />
+        <h1 id={styles.introtext}> 
+        Connect with readers<br />
+        Connect with books<br />
+        Join our community today
+          <form id={styles.signupform} onSubmit={handleSubmit}>
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <button>Join Now</button>
+          </form>
+        </h1>
+        <img id={styles.trackbooks} src={trackbooks} />
+      </section>
+    </main>
   );
 };
 
