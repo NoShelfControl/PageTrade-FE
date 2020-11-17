1101d4c447d9cea30f07d6687608416ff4fdfc1f
Heather Peterson <hpeterson462@gmail.com>
2020-11-16 18:29:14 -0800

    ternary rendering only books to trade in trade div

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import styles from './Home.css';
 import Sidebar from '../sidebar/Sidebar';
 import { getUserBooks } from '../../services/books-api';
@@ -9,31 +10,28 @@ export default function Home() {
   useEffect(() => {
     getUserBooks()
       .then(books => setBooks(books))
-      .then(books => console.log(books))
   }, []);
 
   const booksElements = books.map(book => (
     <ul>
-      <li key={books.id}>
-        <img src={book.image} alt={book.title} />
-        <p>{book.isTradeable}</p>
-      </li>
+      {book.isTradeable === true ?
+        <li key={books.id}>
+          <img src={book.image} alt={book.title} />
+        </li>
+        : null
+      }
     </ul>
   ))
-
-  // if (books.isTradeable === true) {
-  //   return books
-  // }
 
   return (
     <div className={styles.Home} id="outer-container">
       <section className={styles.SideNavBar} id={styles.PageWrap}>
         {/* <Sidebar pageWrapId={'PageWrap'} outerContainerId={'outer-container'} /> */}
         <header>
-          <button>Home</button>
-          <button>Library</button>
-          <button>Requests</button>
-          <button>Profile</button>
+          <Link to="/">Home</Link >
+          <Link to="/library">Library</Link >
+          <Link to="/dashboard">Dashboard</Link >
+          <Link to="/about">About</Link >
         </header>
 
         <main className={styles.Main}>
