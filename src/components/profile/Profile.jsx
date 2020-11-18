d66450c23cb521b5efc2c6f572e256d42808d30a
internetcharles <charlesodellsmith@gmail.com>
2020-11-18 10:50:59 -0800

    can get books from individual profiles!

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import ProfileForm from './ProfileForm';
 import { Link } from 'react-router-dom';
 import { ProfileHook } from '../../hooks/ProfileHook';
