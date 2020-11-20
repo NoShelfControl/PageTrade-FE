23e5bb56114493877cbb1e5bb0c7889d68fc0a39
Charlie Smith <charlesodellsmith@gmail.com>
2020-11-19 16:01:00 -0800

    Feature feed stuff (#20)
    * hello
    
    * profile styling and global user hook
    
    * global feed
    
    * yo
    
    * yes
    
    * feature-feed-stuff

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -6,15 +6,22 @@ import styles from './Home.css';
 import logo from '../../assets/logo.png';
 import { useCurrentUser } from '../../context/AuthContext';
 import Sidebar from '../sidebar/Sidebar';
+import { useGlobalActions } from '../../hooks/ProfileHook';
+import { homeFeedSorter } from '../../utils/feed-sorter';
+import Loading from '../loading/Loading';
 import Footer from '../footer/Footer';
 
 export default function Home() {
   const [books, setSingleBooks] = useState([]);
+  const { loadingActions, globalActions } = useGlobalActions();
+  const sortedActions = homeFeedSorter(globalActions);
 
   useEffect(() => {
     getSingleUserBooks()
       .then(books => setSingleBooks(books));
   }, []);
+
+  if(loadingActions) return <Loading />;
 
   const booksElements = books.map(book => (
     <ul key={books.id}>
@@ -38,10 +45,9 @@ export default function Home() {
     </ul>
   ));
 
-  const user = useCurrentUser();
-
   return (
     <div className={styles.Home}>
+      {console.log(sortedActions)}
       <header>
         <img id={styles.logo} src={logo} />
       </header>
@@ -52,6 +58,11 @@ export default function Home() {
           <div>Wish List {watchListElements}</div>
         </section>
         <div className={styles.feed}>Feed</div>
+        <ul>
+          {sortedActions.map((action, idx) => {
+            return <li key={idx}>{action}</li>;
+          })}
+        </ul>
       </main>
       <Footer />
     </div>
