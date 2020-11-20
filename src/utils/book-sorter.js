b569720157ea332a87b30befef2112eaf8c202e8
internetcharles <charlesodellsmith@gmail.com>
2020-11-20 09:20:59 -0800

    hello?

--- src/utils/book-sorter.js
+++ src/utils/book-sorter.js
@@ -1,6 +1,6 @@
 export const booksSorter = (array) => {
   const sortedFeed = array.slice().reverse();
-  const sortedTradeFeed = array.filter(x => x.isTradeable == true);
+  const sortedTradeFeed = sortedFeed.filter(x => x.isTradeable == true);
   const sortedWatchFeed = sortedFeed.filter(x => x.isWatched == true);
   const slicedSortedTradeFeed = sortedTradeFeed.slice(0, 10);
   const slicedSortedWatchFeed = sortedWatchFeed.slice(0, 10);
